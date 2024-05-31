2daa8ba151a85566c0f98ebb194478c8cf10a854
Nico_264 <84983987+nico264dev@users.noreply.github.com>
2024-06-01 01:22:45 +0200

    Update birthday.js

--- js/birthday.js
+++ js/birthday.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Jun 02, 2024 00:00:00").getTime();
+var countDownDate = new Date("Jun 01, 2024 23:59:59").getTime();
 var countdownfunction = setInterval(function() {
   var now = new Date().getTime();
   var distance = countDownDate - now;
