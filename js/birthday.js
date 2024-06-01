062e0662b99138453ecfcf90b4036145061165fd
Nico_264 <84983987+nico264dev@users.noreply.github.com>
2024-06-02 00:07:35 +0200

    Add files via upload

---
+++ js/birthday.js
@@ -0,0 +1,17 @@
+var countDownDate = new Date("Jun 01, 2025 23:59:59").getTime();
+var countdownfunction = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.querySelector(".days").innerHTML = days;
+  document.querySelector(".hours").innerHTML = hours;
+  document.querySelector(".minutes").innerHTML = minutes;
+  document.querySelector(".seconds").innerHTML = seconds;
+  if (distance < 0) {
+      clearInterval(countdownfunction);
+      document.querySelector(".countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
